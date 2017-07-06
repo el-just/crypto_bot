581ebf5ebaf4fa38f24d9170cb459e1ab121ed79
au.pohila <au.pohila@tensor.ru>
2017-07-06 18:16:27 +0300

    data_base currencies table creation bugfix2

--- bot/server/data_base/create_table_currencies.sql
+++ bot/server/data_base/create_table_currencies.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS ej.currencies (
-	modify_date Date
-	, modify_time DateTime
-	, name String
-	, full_name String
-	, id UInt32
-	, tax_fee Float64
-	, min_conf UInt32
-	, deposit_address String
-	, is_disabled Boolean
-	, is_delisted Boolean
-	, is_frozen Boolean
-	, imprint String
+    modify_date Date
+    , modify_time DateTime
+    , name String
+    , full_name String
+    , id UInt32
+    , tax_fee Float64
+    , min_conf UInt32
+    , deposit_address String
+    , is_disabled UInt8
+    , is_delisted UInt8
+    , is_frozen UInt8
+    , imprint String
 ) ENGINE = MergeTree (modify_date, (modify_date, name, imprint), 8192)
