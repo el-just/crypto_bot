677ec997f5d7b8768c8d498e0a1731298d54ae80
au.pohila <au.pohila@tensor.ru>
2017-09-16 17:18:25 +0300

    dev

--- bot/client/sockets/server.js
+++ bot/client/sockets/server.js
@@ -2,22 +2,22 @@ function ServerSocket () {
   var
     path = 'socket/';
 
-  var socket = new WebSocket("ws://localhost:8081");
+  //var socket = new WebSocket("ws://localhost:8081");
   //   socket.send('asd');
 
   // обработчик входящих сообщений
-  socket.onmessage = function(event) {
-    document.dispatchEvent(new CustomEvent(
-      'chart.data.recieved',
-      { 
-        detail: {
-          data: JSON.parse(event.data)
-        },
-        bubbles: false,
-        cancelable: false
-      }
-    ));
-  };
+  // socket.onmessage = function(event) {
+  //   document.dispatchEvent(new CustomEvent(
+  //     'chart.data.recieved',
+  //     { 
+  //       detail: {
+  //         data: JSON.parse(event.data)
+  //       },
+  //       bubbles: false,
+  //       cancelable: false
+  //     }
+  //   ));
+  // };
 
   this.send = function (payLoad) {
     return new Promise ((resolve, reject) => {
