0cd04e3da7bdfe227b82b4bf2f72dc853efd293c
au.pohila <au.pohila@tensor.ru>
2017-11-23 18:06:39 +0300

    fixed csv format. space

--- deep/init.py
+++ deep/init.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 frame = pd.read_csv ('./data/BTC_USD_20171121.d')
-print (frame.shape)
+
+plt.plot(frame[['timestamp']], frame[['close']], '-')
+plt.show()
--- deep/temp/code_snippets.py
+++ deep/temp/code_snippets.py
@@ -1,17 +1,17 @@
 import http.client
 import json
 
-for date in range (1,22):
+for date in range (1,32):
     day = str(date) if date > 9 else '0'+str(date)
     connect = http.client.HTTPSConnection('cex.io', 443)
-    connect.putrequest('GET', '/api/ohlcv/hd/201711'+day+'/BTC/USD')
+    connect.putrequest('GET', '/api/ohlcv/hd/201710'+day+'/BTC/USD')
     connect.endheaders() 
     response = connect.getresponse ()
 
     data = json.loads (response.read().decode('utf8'))
 
-    f = open ('./data/BTC_USD_201711'+day+'.d', 'w+')
-    f.write ((', ').join (['timestamp', 'open', 'high', 'low', 'close', 'volume'])+'\n')
+    f = open ('./data/BTC_USD_201710'+day+'.d', 'w+')
+    f.write ((',').join (['timestamp', 'open', 'high', 'low', 'close', 'volume'])+'\n')
     for frame in json.loads(data['data1m']):
-        f.write ((', ').join ([str(fr) for fr in frame])+'\n')
+        f.write ((',').join ([str(fr) for fr in frame])+'\n')
     f.close ()
