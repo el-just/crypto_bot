a8a6ec3778336e79915b6e969a40595eb1a4dde8
au.pohila <au.pohila@tensor.ru>
2017-12-01 18:06:15 +0300

    continue plot

--- deep/init.py
+++ deep/init.py
@@ -1,9 +1,34 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import random
 
 frame = pd.read_csv ('./data/BTC_USD_20171121.d')
 
-plt.plot(frame[['timestamp']], frame[['close']], '-')
-plt.show()
+N = 3
+frame['close_avg'] = frame[['close']].shift().rolling(N, min_periods=1).mean()
+frame['timestamp'] = pd.to_datetime (frame['timestamp'], unit='s')
+
+#frame.set_index ('timestamp', inplace=True)
+
+
+#---line
+#plt.plot(frame['timestamp'], frame['close_avg'], '-')
+#plt.show()
+
+#---bar
+#plt.bar(frame.index, frame['volume'])
+#plt.gcf().set_size_inches(12,6)
+#plt.show()
+
+#---line + bar
+# top = plt.subplot2grid((4,4), (0, 0), rowspan=3, colspan=4)
+# top.plot(frame.index, frame['close_avg'], label='Close Avarage')
+# plt.title('BTC_USD_20171121')
+# plt.legend(loc=2)
+# bottom = plt.subplot2grid((4,4), (3,0), rowspan=1, colspan=4)
+# bottom.bar(frame.index, frame['volume'])
+# plt.title('Volume')
+# plt.gcf().set_size_inches(12,8)
+# plt.subplots_adjust(hspace=0.75)
+# plt.show()
+
+#print (frame.head())
