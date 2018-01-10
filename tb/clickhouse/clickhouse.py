9cbb0be06ef184bc72c2c2c63a90963d53628c8c
au.pohila <au.pohila@tensor.ru>
2018-01-10 11:31:08 +0300

    clickhouse-driver

--- tb/clickhouse/clickhouse.py
+++ tb/clickhouse/clickhouse.py
@@ -4,10 +4,10 @@ import datetime
 clickhouse = Client('localhost')
 def insert_tick (tick):
     clickhouse.execute ('''INSERT INTO tb.ticker (tick_date, tick_time, base, quot, close, volume) VALUES''', [{
-        'tick_date' = datetime.datetime.fromtimestamp(tick.at['timestamp']),
-        'tick_time' = datetime.datetime.fromtimestamp(tick.at['timestamp']),
-        'base' = tick.at['base'],
-        'quot' = tick.at['quot'],
-        'close' = tick.at['close'],
-        'volume' = tick.at['volume']
+        'tick_date': datetime.datetime.fromtimestamp(tick.at['timestamp']),
+        'tick_time': datetime.datetime.fromtimestamp(tick.at['timestamp']),
+        'base': tick.at['base'],
+        'quot': tick.at['quot'],
+        'close': tick.at['close'],
+        'volume': tick.at['volume']
         }])
