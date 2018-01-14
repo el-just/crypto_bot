101e22f05bf051b95bc271cf451536fd20b5832e
El <my@email.com>
2018-01-14 15:35:21 +0300

    tools

--- tb/tools.py
+++ tb/tools.py
@@ -9,14 +9,15 @@ from stocks.bitfinex.storage import Storage
 storage = Storage()
 async def get_missing_periods ():
     now = datetime.datetime.now()
-    missing_periods = await storage.get_missing_periods ({
-        'start':time.mktime((now - datetime.timedelta (days=DEFINES.REQUIRED_PERIOD)).timetuple()),
-        'end': time.mktime(now.timetuple())
-        })
+    # missing_periods = await storage.get_missing_periods ({
+    #     'start':time.mktime((now - datetime.timedelta (days=DEFINES.REQUIRED_PERIOD)).timetuple()),
+    #     'end': time.mktime(now.timetuple())
+    #     })
 
+    missing_periods = [{'start': 1508155216.0, 'end': 1515931216.0}]
     if missing_periods is not None:
         for period in missing_periods:
-            print ('{0} - {1}'.format (datetime.datetime.fromtimestamp(period['start'])), datetime.datetime.fromtimestamp(period['end']))
+            print ('{0} - {1}'.format (datetime.datetime.fromtimestamp(period['start']), datetime.datetime.fromtimestamp(period['end']))
     else:
         print ('periods are up to date')
 
