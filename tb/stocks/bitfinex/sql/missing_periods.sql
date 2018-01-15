addf107b7897c730e0c28fd1a33e8b3d1d60d7bd
au.pohila <au.pohila@tensor.ru>
2018-01-15 09:48:05 +0300

    missing_periods

--- tb/stocks/bitfinex/sql/missing_periods.sql
+++ tb/stocks/bitfinex/sql/missing_periods.sql
@@ -1,42 +1,46 @@
-SELECT
-    tick_time,
-    base,
-    quot,
-    runningDifference(tick_time) AS delta
-FROM
-(
+SELECT * FROM (
     SELECT
         tick_time,
         base,
-        quot
+        quot,
+        runningDifference(tick_time) AS delta
+    FROM
+    (
+        SELECT
+            tick_time,
+            base,
+            quot
+        FROM tb.ticker
+        WHERE base='{base}' AND quot='{quot}' AND tick_time >= toDateTime({start}) AND tick_time <= toDateTime({end})
+        ORDER BY tick_time ASC
+    )
+    WHERE delta > {default_miss_time}
+
+    UNION ALL
+
+    SELECT
+        tick_time,
+        base,
+        quot,
+        runningDifference(tick_time) AS delta
     FROM tb.ticker
     WHERE base='{base}' AND quot='{quot}' AND tick_time >= toDateTime({start}) AND tick_time <= toDateTime({end})
     ORDER BY tick_time ASC
+    LIMIT 1
+
+    UNION ALL
+
+    SELECT
+        tick_time,
+        base,
+        quot,
+        runningDifference(tick_time) AS delta
+    FROM tb.ticker
+    WHERE base='{base}' AND quot='{quot}' AND tick_time >= toDateTime({start}) AND tick_time <= toDateTime({end})
+    ORDER BY tick_time DESC
+    LIMIT 1
 )
-WHERE delta > {default_miss_time}
 
-UNION ALL
-
-SELECT
-    tick_time,
-    base,
-    quot,
-    runningDifference(tick_time) AS delta
-FROM tb.ticker
-WHERE base='{base}' AND quot='{quot}' AND tick_time >= toDateTime({start}) AND tick_time <= toDateTime({end})
-ORDER BY tick_time ASC
-LIMIT 1
-
-UNION ALL
-
-SELECT
-    tick_time,
-    base,
-    quot,
-    runningDifference(tick_time) AS delta
-FROM tb.ticker
-WHERE base='{base}' AND quot='{quot}' AND tick_time >= toDateTime({start}) AND tick_time <= toDateTime({end})
-ORDER BY tick_time DESC
-LIMIT 1
+ORDER BY delta ASC, tick_time ASC
 
 FORMAT CSV
