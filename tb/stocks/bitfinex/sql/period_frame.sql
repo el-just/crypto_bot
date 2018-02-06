c4842255fb94253464bab51404729fe94b575124
au.pohila <au.pohila@tensor.ru>
2018-02-06 09:12:04 +0300

    big test first try

--- tb/stocks/bitfinex/sql/period_frame.sql
+++ tb/stocks/bitfinex/sql/period_frame.sql
@@ -1,7 +1,7 @@
 SELECT
     *
 FROM
-    tb.icker
+    tb.ticker
 WHERE
     base='{base}' AND quot='{quot}' AND tick_time >= toDateTime({start}) AND tick_time <= toDateTime({end})
 ORDER BY tick_time ASC
