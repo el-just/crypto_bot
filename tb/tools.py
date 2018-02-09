82afb78c572bb9fe4a2b7420d306c5890adcf401
au.pohila <au.pohila@tensor.ru>
2018-02-09 11:41:10 +0300

    trade emulation

--- tb/tools.py
+++ tb/tools.py
@@ -23,48 +23,34 @@ import matplotlib.pyplot as plt
 window = {'minutes':60}
 
 storage = Storage()
-rest_socket = RESTSocket(storage)
 async def get_missing_periods ():
     now = datetime.datetime.now()
-    missing_periods = await storage.get_missing_periods ({
-        'start':time.mktime((now - datetime.timedelta (days=DEFINES.REQUIRED_PERIOD)).timetuple()),
+    period = {
+        'start':time.mktime((now - datetime.timedelta (**DEFINES.REQUIRED_INTERVAL)).timetuple()),
         'end': time.mktime(now.timetuple())
-        })
-
-    requests = []
-    if missing_periods is not None:
-        for period in missing_periods:
-            print ('{0} - {1}'.format (datetime.datetime.fromtimestamp(period['start']), datetime.datetime.fromtimestamp(period['end'])))
-            for fracted in rest_socket.fract_period (period):
-                requests.append (fracted)
-                print (rest_socket.request_text (fracted))
-
-        print ('About to send {0} requests. Which equals {1} minutes of time'. format (len(requests), len(requests) / 10 - 1))
-
-    else:
-        print ('periods are up to date')
-
-def login ():
-    nonce = int(time.time() * 1000000)
-    auth_payload = 'AUTH{}'.format(nonce)
-    signature = hmac.new(
-        DEFINES.PATTERN.encode(),
-        msg = auth_payload.encode(),
-        digestmod = hashlib.sha384
-        ).hexdigest()
-
-    return {
-        'apiKey': DEFINES.PATH,
-        'event': 'auth',
-        'authPayload': auth_payload,
-        'authNonce': nonce,
-        'authSig': signature
         }
+
+    missing_periods = await storage.get_missing_periods (period)
+
+    print (missing_periods)
+
+    # requests = []
+    # if missing_periods is not None:
+    #     for period in missing_periods:
+    #         print ('{0} - {1}'.format (datetime.datetime.fromtimestamp(period['start']), datetime.datetime.fromtimestamp(period['end'])))
+    #         for fracted in rest_socket.fract_period (period):
+    #             requests.append (fracted)
+    #             print (rest_socket.request_text (fracted))
+
+    #     print ('About to send {0} requests. Which equals {1} minutes of time'. format (len(requests), len(requests) / 10 - 1))
+
+    # else:
+    #     print ('periods are up to date')
+
 
 async def websocket_start ():
     async with websockets.connect('wss://api.bitfinex.com/ws') as websocket:
         await websocket.send(json.dumps({"event":"subscribe", "channel":"ticker", "pair":'btcusd'}))
-        await websocket.send(json.dumps(login()))
         async for message in websocket:
             Logging.log_info (message)
 
