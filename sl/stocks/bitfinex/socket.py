e3d60369f0aa1c95fa4360d0946f2c3aad149d59
au.pohila <au.pohila@tensor.ru>
2018-03-26 17:30:38 +0300

    bitfinex tick formatted

--- sl/stocks/bitfinex/socket.py
+++ sl/stocks/bitfinex/socket.py
@@ -13,6 +13,11 @@ class Socket ():
     _socket = None
     _ws_path = 'wss://api.bitfinex.com/ws'
     _channels = None
+    _tick_buffer = None
+    _last_tick = None
+
+    def __init__ (self):
+        self._tick_buffer = pd.DataFrame(data=[], columns=formats.tick)
 
     async def _subscribe_channels (self):
         try:
@@ -31,30 +36,45 @@ class Socket ():
         channel.name = int(message['chanId'])
         self._channels = self._channels.append (channel)
 
-    async def _resolve_event(self, event_data):
+    async def _assume_event(self, event_data):
         if event_data['event'] == 'subscribed':
             self._register_channel (event_data)
 
-    async def _resolve_tick(self, tick_data):
-        Logger.log_info(tick_data)
-        '''
-        if not self._channels.loc[int(tick[0])].empty:
-            market_name = message['data']['s'].lower()
+    async def _assume_tick(self, tick_data):
+        if not self._channels.loc[int(tick_data[0])].empty:
+            current_date = datetime.datetime.now() 
             tick = pd.Series (
-                data=['bitfinex', int(message['data']['E']) // 1000, market_name, float(message['data']['k']['c']), None, None],
+                data=[
+                    'bitfinex',
+                    int(time.mktime(current_date.timetuple())),
+                    self._channels.loc[int(tick_data[0])].at['name'],
+                    float(tick_data[7]),
+                    None,
+                    None
+                ],
                 index=formats.tick
             )
-            tick.name = time.mktime(datetime.datetime.now().timetuple())
+            tick.name = current_date
 
-            tb_tick = pd.Series (data=tb_data, index=['timestamp', 'base', 'quot', 'close', 'volume'])
-            tb_tick.name = datetime.datetime.fromtimestamp(tb_tick.at['timestamp'])
-'''
+            if self._last_tick is not None:
+                buffer_tick = tick.copy()
+                buffer_tick.at['base_volume'] = float(tick_data[8]) - self._last_tick.at['base_volume'] if float(tick_data[8]) > self._last_tick.at['base_volume'] else 0.
+                self._tick_buffer = self._tick_buffer.append(buffer_tick)
+
+            self._last_tick = tick.copy()
+            self._last_tick.at['base_volume'] = float(tick_data[8])
+
+            if self._tick_buffer.shape[0] > 0 and tick.name - self._tick_buffer.iloc[0].name >= datetime.timedelta(seconds=60):
+                self._tick_buffer = self._tick_buffer.loc[tick.name - datetime.timedelta(seconds=60):, :]
+                tick.at['base_volume'] = self._tick_buffer.loc[:,'base_volume'].sum()
+
+            Logger.log_info(tick)
 
     async def _resolve_message(self, message):
         if type(message) == dict:
-            await self._resolve_event (message)
+            await self._assume_event (message)
         elif type(message) == list and len(message) > 2:
-            await self._resolve_tick (message)
+            await self._assume_tick (message)
 
     async def connect (self):
         try:
@@ -63,7 +83,6 @@ class Socket ():
                 self._clear_channels()
                 await self._subscribe_channels ()
                 async for message in websocket:
-                    Logger.log_info(message)
                     await self._resolve_message (utils.parse_data (message))
         except Exception as e:
             Logger.log_error(e)
