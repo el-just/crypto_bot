f8c38d5c9aa4c4d2e1757d42bb630b716489245a
au.pohila <au.pohila@tensor.ru>
2018-03-29 15:51:01 +0300

    cex socket

---
+++ sl/stocks/cex/__init__.py
@@ -0,0 +1,20 @@
+from common.logger import Logger
+from stocks.cex.socket import Socket
+
+class CEX ():
+    _socket = None
+
+    def __init__ (self):
+        self._socket = Socket ()
+
+    async def ping (self):
+        try:
+            print (await self._socket.get_order_book('BTC-ETH'))
+        except Exception as e:
+            Logger.log_error (e)
+
+    async def custom_action (self):
+        try:
+            await self._socket.connect ()
+        except Exception as e:
+            Logger.log_error (e)
