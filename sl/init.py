54414a3dfa10c1178609ccba5dc6ee95824a7abc
au.pohila <au.pohila@tensor.ru>
2018-03-29 15:50:51 +0300

    cex socket

--- sl/init.py
+++ sl/init.py
@@ -1,13 +1,15 @@
-# import asyncio
+import asyncio
 
-# from common.logger import Logger
-# from stocks.bittrex import Bittrex
-# from stocks.binance import Binance
-# from stocks.bitfinex import Bitfinex
+from common.logger import Logger
+from stocks.bittrex import Bittrex
+from stocks.binance import Binance
+from stocks.bitfinex import Bitfinex
+from stocks.gdax import GDAX
+from stocks.cex import CEX
 
-# try:
-#     loop = asyncio.get_event_loop()
-#     loop.run_until_complete(Bittrex().custom_action())
-#     loop.close()
-# except Exception as e:
-#     Logger.log_error (e)
+try:
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(CEX().custom_action())
+    loop.close()
+except Exception as e:
+    Logger.log_error (e)
