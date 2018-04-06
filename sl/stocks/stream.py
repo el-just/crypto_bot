35f95ae9a1729278f11af8695db9809d164fe66f
au.pohila <au.pohila@tensor.ru>
2018-04-06 17:51:11 +0300

    stream continue

--- sl/stocks/stream.py
+++ sl/stocks/stream.py
@@ -2,20 +2,30 @@ import asyncio
 import websockets
 
 from common.logger import Logger
+import common.formats as formats
 
 class Stream ():
-    _ip = '127.0.0.1'
-    _port = 8765
+    def __init__(self):
+        self.__ip = '127.0.0.1'
+        self.__port = 8765
+        self.__socket = None
 
     async def run (self):
         try:
-            await websockets.serve (self._response, self._ip, self._port)
+            await websockets.serve (self.__listener, self.__ip, self.__port)
+        except Exception as e:
+            Logger.log_error(e)
+
+    async def __resolve_message (self, message):
+        try:
+            await self.__socket.send ('pong' if message == 'ping' else message)
         except Exception as e:
             Logger.log_error(e)
     
-    async def _response (self, websocket, path):
+    async def __listener (self, websocket, path):
         try:
+            self.__socket = websocket
+
             async for message in websocket:
-                await websocket.send ('pong' if message == 'ping' else message)
         except Exception as e:
             Logger.log_error(e)
