6e9ead8cdeb5ae0f4dd2e23b749ab86c8d001f2c
El.Just <el@ej-network>
2018-05-16 19:45:05 +0300

    markup

--- sl/sv/routing/handlers/index.py
+++ sl/sv/routing/handlers/index.py
@@ -6,7 +6,6 @@ import aiohttp_jinja2
 class Index (web.View):
     @aiohttp_jinja2.template('index.html')
     async def get(self):
-        
         exchanges = pd.DataFrame(
                 data=[{'10', '11', '12'}, {'10', '11', '12'}, {'10', '11', '12'}],
                 columns=['bitfinex', 'binance', 'bittrex'],
